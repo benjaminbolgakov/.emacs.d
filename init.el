4d0640da4a3605ed1c71c1df0c91316a40cd7b50
benzel <benjaminbolgakov@gmail.com>
2023-03-10 16:42:22 +0100

    initial restructure, need to update emacs version

--- init.el
+++ init.el
@@ -1,91 +1,62 @@
 ;; TODO:
-;; 1. Separate package-specific settings into separate files: ivy-init.el etc..
-;; 2. autocompletion for lisp, inside this doc for example
-;; 3. Debugging lisp: Running for example "eval-buffer" and recieving an error:
-;;    "eval-buffer: Invalid read syntax: ")" [2 times]" - how to get more info etc?
-;;    REF: https://www.youtube.com/watch?v=LfwSc-lfFxM and https://www.gnu.org/software/emacs/manual/html_node/elisp/Debugging.html
+;; autocompletion for lisp, inside this doc for example
+;; Debugging lisp: Running for example "eval-buffer" and recieving an error:
+;; "eval-buffer: Invalid read syntax: ")" [2 times]" - how to get more info etc?
+
 
 
 ;; This solves the error about retrieving marmalade "incomprehensible buffer"
-(setq gnutls-algorithm-priority "NORMAL:-VERS-TLS1.3")
+;; (setq gnutls-algorithm-priority "NORMAL:-VERS-TLS1.3")
 
-;; Added by Package.el.  This must come before configurations of
-;; installed packages.  Don't delete this line.  If you don't want it,
-;; just comment it out by adding a semicolon to the start of the line.
-;; You may delete these explanatory comments.
-;; Remove GUI bloat
-(tool-bar-mode -1)
-(menu-bar-mode -1)
-(scroll-bar-mode -1)
-(setq inhibit-splash-screen t)
+;; Systems (hostname of my machines)
+(defun mainmint ()
+  (string= (system-name) "mainmint"))
+;; laptop
+;; benzelserver?
 
 
+;; ??????
+;; (setq create-lockfiles nil)
 
-;; Key bindings (General)
-(global-set-key (kbd "C-c a") 'org-agenda)
+;; Start emacs server
+(server-start)
 
+;; set startup dir
+(setq default-directory "~/")
 
-;; Line numbering
-(global-linum-mode t)
-
-;; Disable ear damage
-(setq visible-bell t)
-
-;; Autofollow symlinks
-(setq vc-follow-symlinks t)
-
-;; Disable autosaving
-(setq auto-save-default nil)
-
-;; Use spaces instead of tabs.
-(setq-default indent-tabs-mode nil)
-
-;; Enable highlighting of matching parenteses.
-(show-paren-mode 1)
-
-;; Enable undo/redo for window modifications.
-(winner-mode)
-
-;; Kill trailing whitespace on save.
-(add-hook 'before-save-hook 'delete-trailing-whitespace)
-
-;; Save system clipboard to kill ring before kill.
-(setq save-interprogram-paste-before-kill t)
-
-;; Disable scroll acceleration.
-(setq mouse-wheel-progressive-speed t)
-
-;; Always redraw while scrolling.
-(setq fast-but-imprecise-scrolling nil)
-(setq jit-lock-defer-time 0)
-
-;; Org-Mode
-(use-package org-bullets
-  :ensure t)
+;; set initial buffer at startup
+;; (setq initial-buffer-choice "~/")
 
 
+;; = Paths =
+;; Settings dir
+(setq settings-dir
+	  (expand-file-name "settings" user-emacs-directory))
+;; Functions dir
+(setq functions-dir
+	  (expand-file-name "functions" user-emacs-directory))
+;; List of dirs where separate .el files are stored
+(setq load-dir
+	  (expand-file-name "load" user-emacs-directory))
 
 
-;; Language support
-;; active Babel languages
-(org-babel-do-load-languages
- 'org-babel-load-languages
- '((C . t)))
+(add-to-list 'load-path settings-dir)
+(add-to-list 'load-path functions-dir)
+(add-to-list 'load-path load-dir)
 
-;; Required for projectile-search-replace
-(require 'subr-x)
+;; load base-settings
+(require 'base)
 
-;; M-x package-refresh-contents
-;; M-x package-install <RET> ivy
-;; M-x package-install <RET> ivy-rich
-;; M-x package-install <RET> swiper
-;; M-x package-install <RET> counsel
+
+;; init use-package
 (require 'package)
 (setq package-enable-at-startup nil)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
 (add-to-list 'package-archives '("gnu" . "https://elpa.gnu.org/packages/"))
 (add-to-list 'package-archives '("org" . "https://elpa.gnu.org/packages/"))
 (package-initialize)
+
+;; M-x package-refresh-contents
 
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
@@ -99,130 +70,39 @@
 ;;(require 'use-package)
 ;;(setq use-package-always-ensure t)
 
-;; -------------------------------------------------------------------------
-
-;; [M1]
-(use-package ivy
-  :bind (:map global-map
-              ("C-x c" . ivy-resume))
-  :config
-  (ivy-mode 1))
-
-;; [M2]
-;; (use-package ivy
-;;   :bind ((("C-x c" . ivy-resume))
-;; 		 :config)
-;; (ivy-mode 1) ;; only having this is sufficient for ivy to work?
-
-;; Dashboard on startup
-(use-package dashboard
-  :ensure t
-  :config
-  (dashboard-setup-startup-hook))
-
-(setq dashboard-banner-logo-title "Benzel")
-(setq dashboard-startup-banner "~/.emacs.d/emacs_logo.png")
-(setq dashboard-center-content t)
-(setq dashboard-week-agenda t)
-
-
-(global-set-key (kbd "C-s") 'swiper)
-(global-set-key (kbd "M-x") 'counsel-M-x)
-
-;; package-install projectile-mode
-;; package-install projectile-ripgrep
-(projectile-mode 1)
-(counsel-projectile-mode)
-; bind C-c p som ett prefix för alla projectile-kommandon
-(define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
-
-; Cachea filträdet för projektet (=projectile-find-file blir snabb som attan)
-(setq projectile-enable-caching t)
-; C-p p för att välja ett projekt under denna sökväg
-(setq projectile-project-search-path '("~/"))
-
-
-(add-hook 'emacs-startup-hook 'toggle-frame-maximized)
-
-(setq initial-buffer-choice "~/")
-
-(setq tab-width 4)        ;; set your desired tab width
-(setq indent-tabs-mode t) ;; use tabs for indentation
-
-;; Shell/Bash settings
-;;  ("\\.bash\\'" . sh-mode)
-;; (add-to-list 'auto-mode-alist '("\\.bash_aliases\\." . sh-mode))
-;; # -*- mode: shell-script -*-
-
-;; C/C++ settings
-(setq c-default-style "bsd")
-(setq c-basic-offset 4)
-(setq-default indent-tabs-mode)
-(add-to-list 'auto-mode-alist '("\\.h\\'" . c++-mode))
-
-;;C/C++
-;(setq c-default-style "bsd")
-;(setq c-basic-offset 2)
-;(add-to-list 'auto-mode-alist '("\\.h\\'" . c++-mode))
-
-;;; Initialize 'use-package'.
-;; (require 'package)
-;; (setq package-enable-at-startup nil)
-;; (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
-;; ;; Marmalade is defunct?
-;; ;;(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
-;; (add-to-list 'package-archives '("gnu" . "https://elpa.gnu.org/packages/"))
-;; (package-initialize)
-
-;; (unless (package-installed-p 'use-package)
-;;   (package-refresh-contents)
-;;   (package-install 'use-package))
-
-;; (eval-when-compile
-;;   (require 'use-package))
-
-;; All-The-Icons
-(use-package all-the-icons
-  :if (display-graphic-p))
-
-;; Page-break
+;; Key-binding assistant:
+(require 'bind-key)
+;; Load settings
+(require 'theme-init)
+(require 'ivy-init)
+(require 'docker-init)
+(require 'lsp-init)
+(require 'c++-init)
+(require 'elisp-init)
+(require 'projectile-init)
+(require 'compile-init)
+(require 'org-init)
+(require 'js-init)
+(require 'misc-init)
+(require 'cmake-init)
+(require 'python-init)
+(require 'eshell-init)
+(require 'dired-init)
+(require 'pdf-init)
+(require 'latex-init)
+(require 'avy-init)
+(require 'bash-init)
+(require 'key-bindings)
 
 
 
-;;Theme
-(use-package exotica-theme
-  :config (load-theme 'exotica t))
-
-(use-package pdf-tools
-  :ensure t
-  :config
-  ;; Clear the incompatible modes list to remove nag about
-  ;; global-linum-mode. linum-mode is being disabled for pdf-view-mode
-  ;; a few lines down.
-  (setq pdf-view-incompatible-modes nil)
-  (pdf-loader-install)
-  (defun my/pdf-view-mode-hook ()
-    ;; Disable line numbering in pdf-view-mode.
-    (linum-mode -1))
-
-  (add-hook 'pdf-view-mode-hook 'my/pdf-view-mode-hook))
-
-(use-package pdf-view
-  :ensure nil
-  :after pdf-tools
-  :bind (:map pdf-view-mode-map
-              ;; swiper does not work in pdf-view-mode, use original
-              ;; isearch-forward instead.
-              ("C-s" . isearch-forward)))
-
-(setq inhibit-splash-screen t)
-
-(setq ada-indent 4)
-(setq python-indent-offset 4)
-
-(global-linum-mode t)
 
 
+;; 'package-selected-packages' = explicitly installed packages by user. Get's automatically
+;; updated by Emacs when installing a new package. Used by 'package-autoremove' to decide
+;; which packages are no longer needed. Can be used to (re)install packages on other machines
+;; by running: 'package-install-selected-packages'
+;; Check if package is contained in this list by running: 'package--user-selected-p'
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -231,7 +111,7 @@
  '(org-agenda-files (quote ("~/Documents/agenda_org")))
  '(package-selected-packages
    (quote
-    (org-bullets auctex dashboard all-the-icons page-break-lines magit auto-complete jupyter exotica-theme counsel-projectile grails-projectile-mode pdf-tools use-package))))
+	(modus-themes org-bullets auctex dashboard all-the-icons page-break-lines magit auto-complete jupyter exotica-theme grails-projectile-mode pdf-tools use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
