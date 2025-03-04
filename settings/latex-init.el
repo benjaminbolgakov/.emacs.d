2a4592a83e678ffbb7cf41435b98df8e8222edeb
Benjamin Bolgakov <benjamin.bolgakov@cpacsystems.se>
2025-03-04 22:17:23 +0100

    latex

--- settings/latex-init.el
+++ settings/latex-init.el
@@ -1,14 +1,16 @@
-(use-package auctex
-  :ensure t
-    :hook (LaTeX-mode . hl-todo-mode)
-    :hook (LaTeX-mode . flyspell-mode))
+(use-package tex
+  :ensure auctex)
+;; (use-package auctex
+;;   :ensure t
+;;     :hook (LaTeX-mode . hl-todo-mode)
+;;     :hook (LaTeX-mode . flyspell-mode))
 
-(eval-after-load "xref"
-  '(define-key xref--xref-buffer-mode-map (kbd "M-.") nil))
-(eval-after-load "flyspell" '(define-key flyspell-mode-map (kbd "C-.") nil))
-(eval-after-load "flyspell" '(define-key flyspell-mode-map (kbd "C-,") nil))
-(eval-after-load "latex"
-  '(setq TeX-engine 'omega))
+;; (eval-after-load "xref"
+;;   '(define-key xref--xref-buffer-mode-map (kbd "M-.") nil))
+;; (eval-after-load "flyspell" '(define-key flyspell-mode-map (kbd "C-.") nil))
+;; (eval-after-load "flyspell" '(define-key flyspell-mode-map (kbd "C-,") nil))
+;; (eval-after-load "latex"
+;;   '(setq TeX-engine 'omega))
 
 
 (provide 'latex-init)
