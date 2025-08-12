4c79b0fd2cb98e21d3fca087b7873819be91b1b7
benzel <benzeldeveloper@gmail.com>
2025-08-12 18:27:58 +0200

    testing c++ and lsp-* packages

--- init.el
+++ init.el
@@ -13,15 +13,14 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("3263bd17a7299449e6ffe118f0a14b92373763c4ccb140f4a30c182a85516d7f"
-	 "f681100b27d783fefc3b62f44f84eb7fa0ce73ec183ebea5903df506eb314077"
-	 default))
+   '("3263bd17a7299449e6ffe118f0a14b92373763c4ccb140f4a30c182a85516d7f" "f681100b27d783fefc3b62f44f84eb7fa0ce73ec183ebea5903df506eb314077" default))
  '(elpy-rpc-python-command
    "C:\\\\Users\\\\cid14168\\\\AppData\\\\Local\\\\Programs\\\\Python\\\\Python311\\\\python.exe")
  '(max-lisp-eval-depth 1500)
  '(org-agenda-files nil)
  '(org-image-actual-width '(500))
- '(package-selected-packages nil)
+ '(package-selected-packages
+   '(lsp-mode yasnippet which-key wfnames use-package tide pyvenv python-mode projectile-ripgrep pip-requirements org-bullets moody minions magit lsp-ivy ivy-rich hl-todo highlight-indentation highlight-indent-guides google-translate exotica-theme exec-path-from-shell editorconfig dockerfile-mode docker-compose-mode dashboard counsel-tramp counsel-spotify counsel-projectile cmake-ide cmake-font-lock avy auctex all-the-icons))
  '(python-shell-interpreter "/usr/bin/python3"))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
--- settings/c++-init.el
+++ settings/c++-init.el
@@ -1,70 +1,29 @@
-(use-package cmake-ide
-  :ensure t
-  :init
-  (setq cmake-ide-make-command "make --no-print-directory -j8")
-  :config
-  (cmake-ide-setup))
+;; C/C++ settings
+(setq c-default-style "bsd")
+(setq c-default-style "stroustrup"
+      c-basic-offset 4)
+(setq c-basic-offset 4)
 
-(c-set-offset 'cpp-macro 0 nil)
+;; Associate .h and .hpp files with c++-mode
+(add-to-list 'auto-mode-alist '("\\.h\\'" . c++-mode))
+(add-to-list 'auto-mode-alist '("\\.hpp\\'" . c++-mode))
 
-(defun my/ff-find-other-file-other-window ()
-  (interactive)
-  (ff-find-other-file t))
+(add-to-list 'auto-mode-alist '("\\.c\\'" . c++-mode))
+(add-to-list 'auto-mode-alist '("\\.cpp\\'" . c++-mode))
 
-(defun my/maybe-add-pragma-once ()
-  "Add '#pragma once' to an empty file if its extension is '.hh'"
-  (when (and (string= (file-name-extension (buffer-file-name)) "hh")
-             (= (buffer-size) 0))
-    (insert "#pragma once\n")
-    (set-buffer-modified-p nil)))
+;; https://emacs-lsp.github.io/lsp-ui/
+(use-package lsp-ui
+  :custom
+  (lsp-ui-sideline-update-mode t)
+  (lsp-ui-sideline-delay 2)
+  (lsp-ui-doc-enable t)
+  (lsp-ui-doc-position 'bottom))
 
-(defun my/c++-comment-setup ()
-  "Setup magic multiline C++ comments. M-j for newline with
-multiline comment prefix."
-  (interactive)
-  (setq-local comment-start "/**"
-              comment-end   " */"
-              comment-multi-line t
-              comment-padding nil
-              comment-style 'extra-line
-              comment-continue " * "
-              comment-empty-lines t))
-
-(defun my/c++-mode-hook ()
-  (my/maybe-add-pragma-once)
-  (my/c++-comment-setup)
-
-  (setq c-basic-offset  4
-        c-default-style "linux")
-
-  (local-set-key (kbd "M-o") 'ff-find-other-file)
-  (local-set-key (kbd "M-O") 'my/ff-find-other-file-other-window)
-  (local-set-key (kbd "C-c m") 'cmake-ide-compile)
-  (local-set-key (kbd "C-c i") 'indent-buffer)
-  (local-set-key (kbd "C-c k") 'kattis-submit-and-open-browser)
-  (column-number-mode)
-
-  ;; Bind C-m to 'compile', <return> needs to be bound to
-  ;; 'newline' since it is bound to C-m by default.
-  (cond (window-system
-	 (local-set-key (kbd "<return>") 'newline)
-	 (local-set-key (kbd "C-m") 'compile)))
-
-  (local-set-key (kbd "C-M-k") 'c-doc-comment)
-  (local-set-key (kbd "C-M-j") 'c-block-comment)
-
-  ;; (flycheck-mode)
-  (company-mode)
-  (add-to-list 'company-backends 'company-lsp)
-  (lsp)
-  (lsp-mode))
-
-(add-hook 'c++-ts-mode-hook 'my/c++-mode-hook)
-(add-hook 'c-ts-mode-hook 'my/c++-mode-hook)
 
 (provide 'c++-init)
 
-;; C/C++ settings
+;; LEGACY ========================================================
+
 ;; (defun my-c++-mode-style ()
 ;;   "Custom C++ indentation style."
 ;;   (c-set-style "stroustrup")            ;; Base style
@@ -74,14 +33,6 @@ multiline comment prefix."
 ;;   (setq indent-tabs-mode nil))          ;; Use spaces, not tabs
 
 ;; (add-hook 'c++-mode-hook 'my-c++-mode-style)
-
-;; ;; Associate .h and .hpp files with c++-mode
-;; (add-to-list 'auto-mode-alist '("\\.h\\'" . c++-mode))
-;; (add-to-list 'auto-mode-alist '("\\.hpp\\'" . c++-mode))
-
-;; (provide 'c++-init)
-
-;; LEGACY ========================================================
 
 ;; (setq c-default-style "bsd")
 ;; (setq c-default-style "stroustrup"
